055dae178f509f81819dfbac9c8f644fac0e8fb9
Abraham Galva <ag212@icloud.com>
2019-09-15 23:01:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,35 +1,4 @@
-'#speak_to_grandma'
-'I LOVE YOU GRANDMA' = 'I LOVE YOU TOO PUMPKIN!'
 
 if you say "I LOVE YOU GRANDMA".upcase == 'I LOVE YOU TOO PUMPKIN!'
-describe '#speak_to_grandma' do
-  it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
-    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
-  end
 
-  it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
-    expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
-  end
-
-  it 'responds with HUH?! SPEAK UP, SONNY! for a second time' do
-    expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
-  end
-
-  it 'responds with NO, NOT SINCE 1938! when she can hear you' do
-    expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')).to eq "NO, NOT SINCE 1938!"
-  end
-
-  it 'responds with NO, NOT SINCE 1938! for a second time' do
-    expect(speak_to_grandma('WHAT?')).to eq "NO, NOT SINCE 1938!"
-  end
-
-if 5 > 2
-  print "5 is greater than 2"
-end
-if false
-   puts "This will never get printed because the above
-     statement evaluates to false."
-else
-   puts "This will get printed!"
-end
 
